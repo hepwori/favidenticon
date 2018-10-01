bee5ae1142ef4da295d929244a88a9c74952a141
Isaac Hepworth <isaac.hepworth@gmail.com>
2018-10-01 10:06:11 -0600

    Added drawings; updated all colors

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var docId = document.URL.match(/\/d\/([_A-Za-z0-9-]+)/)[1];
 var docHash = sha1(docId);
 var bgColor;
 switch (document.URL.match(/docs.google.com\/(\w+)/)[1]) {
-	case 'document': bgColor = [70, 136, 241, 255]; break;
-	case 'forms': bgColor = [103, 58, 183, 255]; break;
-	case 'presentation': bgColor = [243, 179, 42, 255]; break;
-	case 'spreadsheets': bgColor = [30, 156, 90, 255];
+	case 'document': bgColor = [81, 142, 245, 255]; break;
+	case 'drawings': bgColor = [222, 83, 71, 255]; break;
+	case 'forms': bgColor = [114, 73, 188, 255]; break;
+	case 'presentation': bgColor = [245, 186, 19, 255]; break;
+	case 'spreadsheets': bgColor = [35, 165, 102, 255];
 }
 var iconBase64 = new Identicon(docHash, { background: bgColor, foreground: [255, 255, 255, 255] }).toString();
 document.querySelector('link[rel*="icon"]').href = 'data:image/png;base64,' + iconBase64;
